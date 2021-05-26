0eed8424db15a1bceadaaca7e1fe043d24f1b76b
= <amittaijoel@outlook.com>
2021-05-26 01:57:19 -0400

    gamestate module v1

---
+++ gamestate.h
@@ -0,0 +1,21 @@
+/**
+ * @file gamestate.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __GAMESTATE_H
+#define __GAMESTATE_H
+
+typedef struct game gamestate_t;
+
+gamestate_t* gamestate_init(char* mapFile);
+
+gamestate_t** gamestate_getPlayers(gamestate_t* game);
+
+#endif
