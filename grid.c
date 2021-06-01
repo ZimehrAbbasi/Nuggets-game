734e0a8d30526f1ce8660eb5a5067afa4fc9ada2
siavava <amittaijoel@outlook.com>
2021-06-01 04:16:34 -0400

    grid module documentation

--- grid.c
+++ grid.c
@@ -1,3 +1,17 @@
+/**
+ * @file grid.c
+ * @author TEAM PINE
+ * @brief: implements functionality for the grid module.
+ * The grid module holds map data and offers
+ * interface functions for checking and updating the data as necessary.
+ * @version 0.1
+ * @date 2021-05-25
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/* standard libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -6,12 +20,25 @@
 #include <float.h>
 #include <math.h>
 
-#include "support/file.h"         /* file operations */
-#include "support/message.h"      /* message operations */
+#include "file.h"         /* file operations */
+#include "message.h"      /* message operations */
 #include "player.h"       /* player module */
 #include "gamestate.h"    /* gamestate module */
 #include "grid.h"         /* self */
 
+
+/**
+ * @brief: function to initialize a grid instance to hold map data.
+ * This function allocates memory, which the caller
+ * must later free by callind grid_delete()
+ * 
+ * Inputs:
+ * @param mapfile: FILE pointer to the source file containing
+ * the map data. It's expected to be open for reading.
+ * 
+ * Returns:
+ * @return grid_t*: pointer to a grid instance containing the map data.
+ */
 grid_t* grid_init(FILE* mapfile) {
 
   if (mapfile != NULL) {
@@ -66,6 +93,19 @@ grid_t* grid_init(FILE* mapfile) {
   return NULL;
 }
 
+
+/**
+ * @brief: function to initialize a grid for a new player.
+ * Rather than loading a player from FILE, 
+ * this function takes in a pointer to a master grid
+ * and copies needed information from that grid instance.
+ * 
+ * Inputs: 
+ * @param masterGrid 
+ * 
+ * Returns:
+ * @return grid_t*: pointer to grid struct
+ */
 grid_t*
 grid_initForPlayer(grid_t* masterGrid)
 {
@@ -96,12 +136,28 @@ grid_initForPlayer(grid_t* masterGrid)
     /* return grid */
     return grid;
   }
-  fprintf(stderr, "Error: cannot init grid to match a NULL grid. Stop. \n");
+
+  /* if master grid is NULL, print error message and return NULL */
+  fprintf(stderr, "Error: cannot init grid to match a NULL grid. Stop.\n");
   return NULL;
 }
 
-char* grid_toString(gamestate_t* state, grid_t* grid){
-  if (grid == NULL){
+
+/**
+ * @brief: function to generate a string representation
+ * of a grid instance.
+ * 
+ * Inputs:
+ * @param state: pointer to gamestate for the current game instance
+ * @param grid: pointer to the grid to be stringified
+ * 
+ * Returns:
+ * @return char*: a string representation of the grid.
+ */
+char* 
+grid_toString(gamestate_t* state, grid_t* grid)
+{
+  if (state == NULL || grid == NULL) {
     return NULL;
   }
   // Get a copy of the master grid
@@ -134,151 +190,321 @@ char* grid_toString(gamestate_t* state, grid_t* grid){
   return stringifiedGrid;
 }
 
+
+/**
+ * @brief: function to generate an identical copy of a grid instance.
+ * 
+ * Inputs: 
+ * @param original_grid 
+ * 
+ * Returns:
+ * @return grid_t*: a pointer to a grid struct 
+ * identical to the one that was passed in as an argument.
+ * 
+ * NOTE: this function allocates memory for the copy of the grid
+ * through calls to other functions. The caller of this function
+ * must later free that pointer by calling grid_delete().
+ */
 grid_t*
-grid_copy(grid_t* original_grid){
+grid_copy(grid_t* originalGrid)
+{
   // Create pointer to new grid object with correct dimensions
-  grid_t* copy = grid_initForPlayer(original_grid);
+  grid_t* copy = grid_initForPlayer(originalGrid);
 
   // Loop over every line in orig. grid and copy into new grid
   for(int y = 0; y < copy->rows; y++){
     // Copy line
-    strcpy(copy->g[y], original_grid->g[y]);
+    strcpy(copy->g[y], originalGrid->g[y]);
   }
 
   return copy;
 }
 
+/**
+ * @brief: function to convert master grid to string.
+ * This function takes in the grid and the gamestate
+ * for the current instance of the game.
+ * This is important because information from the gamestate
+ * is needed to locate and place other players in the
+ * stringified representation of the master grid.
+ * 
+ * Inputs:
+ * @param masterGrid: pointer to the grid instance
+ * representing the master grid for the current session of the game.
+ * @param gamestate: pointer to the gamestate instance
+ * for the current session of the game.
+ * 
+ * Returns:
+ * @return char*: a string representation of the master grid,
+ * with all the player information filled in.
+ * 
+ * NOTE: This function allocates memory for the returned string
+ * via calls to another function. The caller of this function
+ * must later call free() on the returned pointer.
+ * 
+ */
 char* 
-grid_masterGridToString(grid_t* grid, gamestate_t* gamestate)
+grid_masterGridToString(grid_t* masterGrid, gamestate_t* state)
 {
-  char* map = grid_toString(gamestate, grid);
-  int cols = grid_getColumns(grid);
 
-  player_t** players = gamestate_getPlayers(gamestate);
-  for (int i = 0; i<25; i++) {
-    if (players[i] != NULL) {
-      int x = player_getX(players[i]);
-      int y = player_getY(players[i]);
-      char letter = player_getLetter(players[i]);
-      int pos = cols*(y) + y + x;            /* (length of previous cols) + ("\n" ) + pos in current row */ 
+  /* only doable if both pointers are not NULL */
+  if (masterGrid != NULL && state != NULL) {
 
-      map[pos] = letter;
+    /* create string version of grid */
+    char* map = grid_toString(state, masterGrid);
+
+    /* get number of columns */
+    int cols = grid_getColumns(masterGrid);
+
+    /* get players in game */
+    player_t** players = gamestate_getPlayers(state);
+
+    /* loop over all possible players */
+    for (int i = 0; i<26; i++) {
+
+      /* if player is not NULL,
+         get position and add them to map. */
+      if (players[i] != NULL) {
+        int x = player_getX(players[i]);
+        int y = player_getY(players[i]);
+        char letter = player_getLetter(players[i]);
+
+        /* position = (length of previous rows) + 
+           (total "\n" appended ) + 
+           pos in current row */ 
+        int pos = cols*(y) + y + x;            
+
+        /* set position in string to represent a player */
+        map[pos] = letter;
+      }
     }
+
+    /* return constructed representation */
+    return map;
   }
-  return map;
+
+  /* if either pointer is NULL, print error message
+     and return NULL */
+  fprintf(stderr, "Attempt to convert NULL master grid or gamestate. Stop.\n");
+  return NULL;
+  
 }
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * is a player.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holdint map data.
+ * @param x: x position. of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: given point is a wall.
+ * @return false: given point is NOT a wall.
+ */
 bool
 grid_isWall(grid_t* grid, int x, int y)
 {
-  char **master = grid->g;
-  return ( master[y][x] == '|' || 
-           master[y][x] == '-' || 
-           master[y][x] == '+' || 
-           master[y][x] == ' ');
-        // return true;
-    // }
-    // return false;
+  if (grid != NULL) {
+    char **master = grid->g;
+    return (master[y][x] == '|' || master[y][x] == '-' || 
+            master[y][x] == '+' || master[y][x] == ' ');
+  }
+
+  /* if grid is NULL,
+     print error and return false. */
+  fprintf(stderr, "Error: attempt to check position in NULL grid. Stop.\n");
+  return false;
 }
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * is a player.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holding map data.
+ * @param x: x position. of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: there's a player at the given point.
+ * @return false: there's no player at the given point.
+ */
 bool
 grid_isPlayer(grid_t* grid, int x, int y)
 {
-  char **master = grid->g;
-  return isalpha(master[y][x]);
-    // {
-    //     return true;
-    // }
-    // return false;
+  if (grid != NULL) {
+    char **master = grid->g;
+    return isalpha(master[y][x]);
+  }
+
+  /* if grid is NULL,
+     print error and return false. */
+  fprintf(stderr, "Error: attempt to check position in NULL grid. Stop.\n");
+  return false;
 }
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * contains gold.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holding map data.
+ * @param x: x position. of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: given point contains gold.
+ * @return false: given point does NOT contain gold.
+ */
 bool
 grid_isGold(grid_t* grid, int x, int y)
 {
-  char **master = grid->g;
-  return master[y][x] == '*';
-    // {
-    //     return true;
-    // }
-    // return false;
+
+  if (grid != NULL) {
+    char **master = grid->g;
+    return master[y][x] == '*';
+  }
+
+  /* if grid is NULL,
+     print error and return false. */
+  fprintf(stderr, "Error: attempt to check position in NULL grid. Stop.\n");
+  return false;
 }
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * is a passage.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holdint map data.
+ * @param x: x position of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: given point is a passage.
+ * @return false: given point is NOT a passage.
+ */
 bool
 grid_isPassage(grid_t* grid, int x, int y)
 {
+  
   if (grid != NULL) {
-    char **master = grid->g;
+    char** master = grid->g;
     return master[y][x] == '#';
   }
   return -1;
-    // {
-    //     return true;
-    // }
-    // return false;
 }
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * is a playable space.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holdint map data.
+ * @param x: x position. of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: given point is a wall.
+ * @return false: given point is NOT a wall.
+ */
 bool
 grid_isSpace(grid_t* grid, int x, int y) {
   char **master = grid->g;
   return master[y][x] == '.';
 }
 
-bool grid_canMove(grid_t* Grid, player_t* player, char k){
-    char letter = k;
-    switch (letter) {
-    case 'l': 
-        if(!grid_isWall(Grid, player->x+1, player->y)){
-            return true;
-        }
-        break;
-    case 'h': 
-        if(!grid_isWall(Grid, player->x-1, player->y)){
-            return true;
-        }
-        break;
-    case 'k': 
-        if(!grid_isWall(Grid, player->x, player->y+1)){
-            return true;
-        }
-        break;
-    case 'j': 
-        if(!grid_isWall(Grid, player->x, player->y-1)){
-            return true;
-        }
-        break;
-    case 'u': 
-        if(!grid_isWall(Grid, player->x+1, player->y+1)){
-            return true;
-        }
-        break;
-    case 'y': 
-        if(!grid_isWall(Grid, player->x-1, player->y+1)){
-            return true;
-        }
-        break;
-    case 'b': 
-        if(!grid_isWall(Grid, player->x-1, player->y-1)){
-            return true;
-        }
-        break;
-    case 'n': 
-        if(!grid_isWall(Grid, player->x+1, player->y-1)){
-            return true;
-        }
-        break;
-    default:
-        return false;
+/**
+ * @brief: function to check if a player can move in some
+ * direction given a key.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct representing the map.
+ * @param player: pointer to a player trying to move.
+ * @param k: action character that triggered the call.
+ * 
+ * Returns:
+ * @return true: the next point in given direction is a valid move.
+ * @return false: the next point in given direction NOT a valid move.
+ */
+bool 
+grid_canMove(grid_t* grid, player_t* player, char k)
+{
+  char letter = k;
+  switch (letter) {
+  case 'l': 
+    if(!grid_isWall(grid, player->x+1, player->y)){
+      return true;
     }
+    break;
+  case 'h': 
+    if(!grid_isWall(grid, player->x-1, player->y)){
+      return true;
+    }
+    break;
+  case 'k': 
+    if(!grid_isWall(grid, player->x, player->y+1)){
+      return true;
+    }
+    break;
+  case 'j': 
+    if(!grid_isWall(grid, player->x, player->y-1)){
+      return true;
+    }
+    break;
+  case 'u': 
+    if(!grid_isWall(grid, player->x+1, player->y+1)){
+      return true;
+    }
+    break;
+  case 'y': 
+    if(!grid_isWall(grid, player->x-1, player->y+1)){
+      return true;
+    }
+    break;
+  case 'b': 
+    if(!grid_isWall(grid, player->x-1, player->y-1)){
+      return true;
+    }
+    break;
+  case 'n': 
+    if(!grid_isWall(grid, player->x+1, player->y-1)){
+      return true;
+    }
+    break;
+  default:
     return false;
+  }
+  return false;
 }
 
+/**
+ * @brief: static function to calculate the slope
+ * of the direct line of vision between two points
+ * in the grid.
+ * 
+ * @param x1: coordinate x of first point.
+ * @param y1: coordinate y of first point. 
+ * @param x2: coordinate x of second point. 
+ * @param y2: coordinate y of second point. 
+ * @return double: value of slope of 
+ * direct line of sight
+ * from point one to point two
+ */
 static double calculate_slope(int x1, int y1, int x2, int y2){
 
-    if(y1 == y2){
-        return DBL_MAX;
-    }
+  if (y1 == y2) {
+    return DBL_MAX;
+  }
 
-    return ((double)(x1 - x2))/((double)(y1 - y2));
+  return ((double)(x1 - x2))/((double)(y1 - y2));
 }
 
 static double func(int x, int y, double slope){
@@ -440,6 +666,18 @@ void grid_movePlayer(gamestate_t* gameState, player_t* player, int x, int y){
 	}
 }
 
+/**
+ * @brief: function to delete a grid instance 
+ * and free all allocated memory.
+ * The grid struct should have been created
+ * via a call to either grid_init()
+ * or grid_initForPlayer()
+ * 
+ * Inputs:
+ * @param grid: the grid instance
+ * 
+ * Returns: None.
+ */
 void
 grid_delete(grid_t* grid) {
   if (grid != NULL) {
@@ -451,9 +689,24 @@ grid_delete(grid_t* grid) {
   }
 }
 
+
+/**
+ * @brief: function to get the number of rows in a grid instance.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid instance
+ * 
+ * Returns:
+ * @return int: number of rows.
+ * NOTE: we return -1 as a flag for a NULL grid.
+ * The caller may check that conditions 
+ * in the return value to detect errors.
+ */
 int
 grid_getRows(grid_t* grid)
 {
+  /* cannot get number of rows
+     from NULL grid */
   if (grid != NULL) {
     return grid->rows;
   }
@@ -461,9 +714,24 @@ grid_getRows(grid_t* grid)
   return -1;
 }
 
+
+/**
+ * @brief: function to get the number of columns in a grid instance.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid instance
+ * 
+ * Returns:
+ * @return int: number of columns.
+ * NOTE: we return -1 as a flag for a NULL grid.
+ * The caller may check that conditions 
+ * in the return value to detect errors.
+ */
 int
 grid_getColumns(grid_t* grid)
 {
+  /* cannot get number of columns
+     from NULL grid */
   if (grid != NULL) {
     return grid->cols;
   }
@@ -471,12 +739,25 @@ grid_getColumns(grid_t* grid)
   return -1;
 }
 
+/**
+ * @brief: function to get the raw representation
+ * of the map from a grid struct.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct.
+ * @return char**: an array of sentences (2D char array)
+ * NOTE: we do not allocate memory for the return value.
+ * The caller may NOT free the pointer returned herein.
+ */
 char**
 grid_getGrid(grid_t* grid)
 {
+  /* cannot get raw representation
+     from NULL grid */
   if (grid != NULL) {
     return grid->g;
   }
+  /* print error message in the case of NULL pointer */
   fprintf(stderr, "Attempt to get grid representation in a NULL grid. Stop.\n");
   return NULL;
 }
--- grid.h
+++ grid.h
@@ -1,7 +1,9 @@
 /**
  * @file grid.h
- * @author your name (you@domain.com)
- * @brief 
+ * @author TEAM PINE
+ * @brief: exports functionality for the grid module.
+ * The grid module holds map data and offers
+ * interface functions for checking and updating the data as necessary.
  * @version 0.1
  * @date 2021-05-25
  * 
@@ -21,6 +23,9 @@
 #include <float.h>
 #include <math.h>
 
+#include "file.h"         /* file operations */
+#include "message.h"      /* message operations */
+
 typedef struct grid {
   char** g;
   int rows;
@@ -30,34 +35,246 @@ typedef struct grid {
 typedef struct player player_t;
 typedef struct game gamestate_t;
 
+
+/**
+ * @brief: function to initialize a grid instance to hold map data.
+ * This function allocates memory, which the caller
+ * must later free by callind grid_delete()
+ * 
+ * Inputs:
+ * @param mapfile: FILE pointer to the source file containing
+ * the map data. It's expected to be open for reading.
+ * 
+ * Returns:
+ * @return grid_t*: pointer to a grid instance containing the map data.
+ */
 grid_t* grid_init(FILE* mapfile);
 
+
+/**
+ * @brief: function to initialize a grid for a new player.
+ * Rather than loading a player from FILE, 
+ * this function takes in a pointer to a master grid
+ * and copies needed information from that grid instance.
+ * 
+ * Inputs: 
+ * @param masterGrid 
+ * 
+ * Returns:
+ * @return grid_t*: pointer to grid struct
+ */
 grid_t* grid_initForPlayer(grid_t* masterGrid);
 
+
+/**
+ * @brief: function to delete a grid instance 
+ * and free all allocated memory.
+ * The grid struct should have been created
+ * via a call to either grid_init()
+ * or grid_initForPlayer()
+ * 
+ * Inputs:
+ * @param grid: the grid instance
+ * 
+ * Returns: None.
+ */
 void grid_delete(grid_t* grid);
 
+
+/**
+ * @brief: function to get the number of rows in a grid instance.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid instance
+ * 
+ * Returns:
+ * @return int: number of rows.
+ * NOTE: we return -1 as a flag for a NULL grid.
+ * The caller may check that conditions 
+ * in the return value to detect errors.
+ */
 int grid_getRows(grid_t* grid);
 
+
+/**
+ * @brief: function to get the number of columns in a grid instance.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid instance
+ * 
+ * Returns:
+ * @return int: number of columns.
+ * NOTE: we return -1 as a flag for a NULL grid.
+ * The caller may check that conditions 
+ * in the return value to detect errors.
+ */
 int grid_getColumns(grid_t* grid);
 
+
+/**
+ * @brief: function to get the raw representation
+ * of the map from a grid struct.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct.
+ * @return char**: an array of sentences (2D char array)
+ * NOTE: we do not allocate memory for the return value.
+ * The caller may NOT free the pointer returned herein.
+ */
 char** grid_getGrid(grid_t* grid);
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * is a player.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holding map data.
+ * @param x: x position. of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: given point is a wall.
+ * @return false: given point is NOT a wall.
+ */
 bool grid_isWall(grid_t* grid, int x, int y);
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * is a player.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holding map data.
+ * @param x: x position. of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: there's a player at the given point.
+ * @return false: there's no player at the given point.
+ */
 bool grid_isPlayer(grid_t* grid, int x, int y);
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * contains gold.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holding map data.
+ * @param x: x position. of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: given point contains gold.
+ * @return false: given point does NOT contain gold.
+ */
 bool grid_isGold(grid_t* grid, int x, int y);
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * is a passage.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holdint map data.
+ * @param x: x position of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: given point is a passage.
+ * @return false: given point is NOT a passage.
+ */
 bool grid_isPassage(grid_t* grid, int x, int y);
 
+
+/**
+ * @brief: function to check if a given point in the grid
+ * is a playable space.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct holdint map data.
+ * @param x: x position. of point to check in the map.
+ * @param y: y position of point to check in the map.
+ * 
+ * Returns:
+ * @return true: given point is a wall.
+ * @return false: given point is NOT a wall.
+ */
 bool grid_isSpace(grid_t* grid, int x, int y);
 
+
+/**
+ * @brief: function to check if a player can move in some
+ * direction given a key.
+ * 
+ * Inputs:
+ * @param grid: pointer to a grid struct representing the map.
+ * @param player: pointer to a player trying to move.
+ * @param k: action character that triggered the call.
+ * 
+ * Returns:
+ * @return true: the next point in given direction is a valid move.
+ * @return false: the next point in given direction NOT a valid move.
+ */
 bool grid_canMove(grid_t* master, player_t* player, char k);
 
 void grid_calculateVisibility(grid_t* Grid, player_t* player);
 
+/**
+ * @brief: function to convert master grid to string.
+ * This function takes in the grid and the gamestate
+ * for the current instance of the game.
+ * This is important because information from the gamestate
+ * is needed to locate and place other players in the
+ * stringified representation of the master grid.
+ * 
+ * Inputs:
+ * @param masterGrid: pointer to the grid instance
+ * representing the master grid for the current session of the game.
+ * @param gamestate: pointer to the gamestate instance
+ * for the current session of the game.
+ * 
+ * Returns:
+ * @return char*: a string representation of the master grid,
+ * with all the player information filled in.
+ * 
+ * NOTE: This function allocates memory for the returned string
+ * via calls to another function. The caller of this function
+ * must later call free() on the returned pointer.
+ * 
+ */
+char* grid_masterGridToString(grid_t* masterGrid, gamestate_t* state);
+
+
+/**
+ * @brief: function to generate an identical copy of a grid instance.
+ * 
+ * Inputs: 
+ * @param original_grid 
+ * 
+ * Returns:
+ * @return grid_t*: a pointer to a grid struct 
+ * identical to the one that was passed in as an argument.
+ * 
+ * NOTE: this function allocates memory for the copy of the grid
+ * through calls to other functions. The caller of this function
+ * must later free that pointer by calling grid_delete().
+ */
 grid_t* grid_copy(grid_t* original_grid);
 
+
+/**
+ * @brief: function to generate a string representation
+ * of a grid instance.
+ * 
+ * Inputs:
+ * @param state: pointer to gamestate for the current game instance
+ * @param grid: pointer to the grid to be stringified
+ * 
+ * Returns:
+ * @return char*: a string representation of the grid.
+ */
 char* grid_toString(gamestate_t* state, grid_t* grid);
 
 #endif /* __GRID_H */
